56e30e82c33334564a4066fc0f2c9c978cb8ef92
IvailoManolov <ivomoreras@abv.bg>
2023-08-26 10:55:37 +0300

    Async user hook to get details and subscription

---
+++ spotify-clone/hooks/useUser.tsx
@@ -0,0 +1,87 @@
+import { Subscription, UserDetails } from "@/types";
+
+import { User } from "@supabase/auth-helpers-nextjs";
+
+import {
+    useSessionContext,
+    useUser as useSupaUser
+} from "@supabase/auth-helpers-react";
+
+import { createContext, useEffect, useState } from "react";
+
+type UserContextType = {
+    accessToken: string | null;
+    user: User;
+    userDetails: UserDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export interface Props {
+    [propName: string]: any;
+};
+
+export const MyUserContextProvider = (props: Props) => {
+    const {
+        session,
+        isLoading: isLoadingUser,
+        supabaseClient: supabase
+    } = useSessionContext();
+
+    const user = useSupaUser();
+
+    const accessToken = session?.access_token ?? null;
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+    const [subscriptionDetails, setSubscriptionDetails] = useState<Subscription | null>(null);
+
+    const getUserDetails = async () => {
+        return await supabase.from('users').select('*').single();
+    }
+
+    const getSubscription = async () => {
+        return await supabase
+            .from('subscriptions')
+            .select('*, prices(*,products(*))')
+            .in('status', ['trialing', 'active'])
+            .single();
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            if (user && !isLoading && !userDetails && !subscriptionDetails) {
+                setIsLoading(true);
+
+                const userDetails = await getUserDetails();
+                const subscriptionDetails = await getSubscription();
+
+                if (userDetails) {
+                    setUserDetails(userDetails.data);
+                }
+                else {
+                    console.log("Problem with accquiring user details");
+                }
+
+                if (subscriptionDetails) {
+                    setSubscriptionDetails(subscriptionDetails.data);
+                }
+                else {
+                    console.log("Problem with accquiring user subscription");
+                }
+
+            }
+            else if (!user && !isLoadingUser && !isLoading) {
+                setUserDetails(null);
+                setSubscriptionDetails(null);
+            }
+        }
+
+
+        fetchData();
+
+    }, [user, isLoadingUser]);
+}
