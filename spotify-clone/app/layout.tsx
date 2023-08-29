40818550986a94c66f53b23fdc8359620e46e775
IvailoManolov <ivomoreras@abv.bg>
2023-08-29 22:09:06 +0300

    --loading on song upload

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
